6c79be4cc39d722784a06d2a9b8de065d1683431
Антон Талашманов <talashmanovhm@gmail.com>
2019-05-13 10:49:03 +0500

    Edited shared markup info

--- Printers/Controllers/HomeController.cs
+++ Printers/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace Printers.Controllers
@@ -11,6 +15,19 @@ namespace Printers.Controllers
     public class HomeController : Controller
     {
         string constr = ConfigurationManager.ConnectionStrings["DC"].ConnectionString;
+
+        public ActionResult Movement()
+        {
+            var movement = new List<MovementView>();
+
+            using (IDbConnection db = new SqlConnection(constr))
+            {
+                movement = db.Query<MovementView>("select * from dbo.MovementView order by id desc").ToList();
+            }
+            ViewBag.Message = "История джижения техники. Где сейчас находится принтер, кто и когда его туда перенес.";
+
+            return View(movement);
+        }
 
         public ActionResult Index()
         {
